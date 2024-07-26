3e0d66d1630ad729f0f7c0a6a63c8f3d39b8382d
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-07-25 20:57:19 -0400

    update: prompts order

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,13 +1,5 @@
 const prompts: Array<IPrompts> = [
     {
-        name: 'email',
-        message: 'What is your email address?'
-    },
-    {
-        name: 'github',
-        message: 'What is your GitHub username?'
-    },
-    {
         name: 'name',
         message: 'What is your project name?',
     },
@@ -50,7 +42,15 @@ const prompts: Array<IPrompts> = [
         default: 'None',
         type: 'list',
         choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
-    }
+    },
+    {
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
 ];
 
 export default prompts;
