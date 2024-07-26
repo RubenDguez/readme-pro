c53cdb1db603a6c216a2eaec2568a5566951a3ef
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-07-25 23:20:33 -0400

    add: badges section

--- src/Generate.ts
+++ src/Generate.ts
@@ -37,9 +37,12 @@ export default class Generate {
      * @return {ILicense}
      * @description Method to generate the license badge and link
      */
-    private genLicense(license: TLicense): ILicense {
-        const urls: Record<TLicense, string> = {
-            'None': 'https://opensource.org/license',
+    private genLicense(license: TLicense): string {
+        if (license === 'None') {
+            return '[This project has no license yet, please click on this link in order to better understand licensing and perhaps choose one](https://opensource.org/license)'
+        }
+
+        const urls: Record<Exclude<TLicense, 'None'>, string> = {
             'Apache License 2.0': 'https://opensource.org/license/apache-2-0',
             'GNU General Public License v3.0': 'https://opensource.org/license/gpl-3-0',
             'MIT License': 'https://opensource.org/license/mit',
@@ -58,10 +61,34 @@ export default class Generate {
         const colors = ['blue', 'orange', 'green', 'yellow', 'red'];
         const color = colors[Math.floor(Math.random() * colors.length)];
 
-        const badge = license !== 'None' ? `![badge](https://img.shields.io/badge/License-${license.replace(/\s+|-/g, '_')}-${color})` : '';
-        const link = license !== 'None' ? `[${license}](${urls[license]})` : `[This project has no license yet, please follow this link in order to better understand licensing and perhaps choose one](${urls[license]})`;
+        return `[![](https://img.shields.io/badge/License-${license.replace(/\s+|-/g, '_')}-${color})](${urls[license]})`;
+    }
 
-        return {badge, link};
+    /**
+     * Get Badges
+     * @param {Array<TBadges>} badges
+     * @return {string}
+     */
+    private getBadges(badges: Array<TBadges>): string {
+        if (!badges) return '';
+
+        const urls: Record<TBadges, string> = {
+            'CSS': '![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)',
+            'Express': '![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)',
+            'HTML': '![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)',
+            'JavaScript': '![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)',
+            'Node': '![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)',
+            'React': '![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)',
+            'Redux': '![Redux](https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white)',
+            'TypeScript': '![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)'
+        }
+        
+        let str = '';
+        for (const badge of badges) {
+            str = str.concat(urls[badge]).concat('\n');
+        }
+
+        return str;
     }
 
     /**
@@ -98,27 +125,24 @@ export default class Generate {
         installation,
         usage,
         license,
+        badges
     }: ICreateOptions): Generate {
-        if (!email) throw new Error('The email address is a required field');
-        if (!github) throw new Error('The GitHub username is a required field');
-        if (!name) throw new Error('The title is a required field');
+        const _installation = this.getInstallation(installation);
+        const _license = this.genLicense(license);
+        const _badges = this.getBadges(badges);
 
         this._template = this._template.replace('<project-title>', name);
-
         this._template = this._template.replace('<project-description>', description);
         this._template = this._template.replace('<project-motivation>', motivation ? '- Project motivation: ' + motivation : '');
         this._template = this._template.replace('<project-build>', build ? '- Why building this project? ' + build : '');
         this._template = this._template.replace('<project-solve>', solve ? '- What problem does it solve? ' + solve : '');
         this._template = this._template.replace('<project-learning>', learn ? '- What did you learn? ' + learn : '');
-        this._template = this._template.replace('<project-installation>', this.getInstallation(installation));
+        this._template = this._template.replace('<project-installation>', _installation);
         this._template = this._template.replace('<project-usage>', usage);
-
-        const lic = this.genLicense(license);
-        this._template = this._template.replace('<project-license-badge>', lic.badge);
-        this._template = this._template.replace('<project-license>', lic.link);
-
+        this._template = this._template.replace('<project-license-badge>', _license);
         this._template = this._template.replace('<github-profile>', `- This is my GitHub profile: [${github}](https://github.com/${github})`);
         this._template = this._template.replace('<email-address>', `- If you have further questions, you can contact me at: ${email}`);
+        this._template = this._template.replace('<project-badges>', _badges);
 
         this._template = this.clean();
 
