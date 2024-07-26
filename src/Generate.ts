4c2c51b21a4a86b811f8083a48ce96723f04f43a
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-07-25 20:57:00 -0400

    update: generate class
    add: genLicense method to generate the license badge and link
    update: types
    update: jsdoc

--- src/Generate.ts
+++ src/Generate.ts
@@ -14,7 +14,7 @@ export default class Generate {
     /**
      * Clean
      * @return {string}
-     * @description Function to clean the README.md file
+     * @description Method to clean the README.md file
      */
     private clean(): string {
         let text = '';
@@ -33,13 +33,46 @@ export default class Generate {
     }
 
     /**
-     * Get Installation
-     * @param {string} installation 
-     * @return {string}
-     * @description Function to get the installation command
+     * Generate License
+     * @param {TLicense} license
+     * @return {ILicense}
+     * @description Method to generate the license badge and link
      */
-    private getInstallation(installation: string): string {
-        const install: Record<string, string> = {
+    private genLicense(license: TLicense): ILicense {
+        const urls: Record<TLicense, string> = {
+            'None': 'https://opensource.org/license',
+            'Apache License 2.0': 'https://opensource.org/license/apache-2-0',
+            'GNU General Public License v3.0': 'https://opensource.org/license/gpl-3-0',
+            'MIT License': 'https://opensource.org/license/mit',
+            'BSD 2-Clause "Simplified" License': 'https://opensource.org/license/bsd-2-clause',
+            'BSD 3-Clause "New" or "Revised" License': 'https://opensource.org/license/bsd-3-clause',
+            'Boost Software License 1.0': 'https://opensource.org/license/bsl-1-0',
+            'Creative Commons Zero v1.0 Universal': 'https://opensource.org/license/0bsd',
+            'Eclipse Public License 2.0': 'https://opensource.org/license/epl-2-0',
+            'GNU Affero General Public License v3.0': 'https://opensource.org/license/agpl-v3',
+            'GNU General Public License v2.0': 'https://opensource.org/license/gpl-2-0',
+            'GNU Lesser General Public License v2.1': 'https://opensource.org/license/lgpl-2-1',
+            'Mozilla Public License 2.0': 'https://opensource.org/license/mpl-2-0',
+            'The Unlicense': 'https://opensource.org/license/unlicense',
+        };
+
+        const colors = ['blue', 'orange', 'green', 'yellow', 'red'];
+        const color = colors[Math.floor(Math.random() * colors.length)];
+
+        const badge = license !== 'None' ? `![badge](https://img.shields.io/badge/License-${license.replace(/\s+|-/g, '_')}-${color})` : '';
+        const link = license !== 'None' ? `[${license}](${urls[license]})` : `[This project has no license yet, please follow this link in order to better understand licensing and perhaps choose one](${urls[license]})`;
+
+        return {badge, link};
+    }
+
+    /**
+     * Get Installation
+     * @param {TInstallation} installation 
+     * @return {string}
+     * @description Method to get the installation command
+     */
+    private getInstallation(installation: TInstallation): string {
+        const install: Record<TInstallation, string> = {
             npm: '```sh\nnpm install\n```',
             yarn: '```sh\nyarn install\n```',
             bun: '```sh\nbun install\n```',
@@ -52,7 +85,7 @@ export default class Generate {
      * Setup
      * @param {ICreateOptions} options  
      * @return {Generate}
-     * @description Function to setup the README.md file
+     * @description Method to setup the README.md file
      */
     setup({
         email,
@@ -80,8 +113,11 @@ export default class Generate {
         this._template = this._template.replace('<project-learning>', learn ? '- What did you learn? ' + learn : '');
         this._template = this._template.replace('<project-installation>', this.getInstallation(installation));
         this._template = this._template.replace('<project-usage>', usage);
-        this._template = this._template.replace('<project-license-badge>', `![badge](https://img.shields.io/badge/${license.replace(/\s+/g, '_')}-orange)`);
-        this._template = this._template.replace('<project-license>', license);
+
+        const lic = this.genLicense(license);
+        this._template = this._template.replace('<project-license-badge>', lic.badge);
+        this._template = this._template.replace('<project-license>', lic.link);
+
         this._template = this._template.replace('<github-profile>', `- This is my GitHub profile: [${github}](https://github.com/${github})`);
         this._template = this._template.replace('<email-address>', `- If you have further questions, you can contact me at: ${email}`);
 
@@ -93,7 +129,7 @@ export default class Generate {
     /**
      * Create
      * @return {boolean}
-     * @description Function to create the README File
+     * @description Method to create the README File
      */
     create(): boolean {
         try {
