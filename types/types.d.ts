ede23653a1c30a90ac9a4de488fd303cc7ecf756
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-07-25 22:11:41 -0400

    update: extracting interfaces from types file

--- types/types.d.ts
+++ types/types.d.ts
@@ -42,30 +42,3 @@ type TInstallation =
     'npm' |
     'yarn' |
     'bun'
-
-interface ICreateOptions {
-    email: string;
-    github: string;
-    name: string;
-    description: string;
-    motivation: string;
-    build: string;
-    solve: string;
-    learn: string;
-    installation: TInstallation;
-    usage: string;
-    license: TLicense;
-}
-
-interface IPrompts {
-    name: TPromptName,
-    message: string,
-    default?: string,
-    type?: TPromptType,
-    choices?: Array<TLicense> | Array<TInstallation>
-}
-
-interface ILicense { 
-    badge: string,
-    link: string
-}
