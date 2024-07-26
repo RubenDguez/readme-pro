828f87b8d2d3e440141b5d8b0bccd3f888cc0d57
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-07-25 20:58:17 -0400

    update: types
    add: TLicense, ILicense

--- types.d.ts
+++ types.d.ts
@@ -11,7 +11,7 @@ type TPromptName =
     'usage' |
     'license';
 
-type TPromptType = 
+type TPromptType =
     'input' |
     'number' |
     'confirm' |
@@ -22,6 +22,27 @@ type TPromptType =
     'password' |
     'editor'
 
+type TLicense =
+    'None' |
+    'Apache License 2.0' |
+    'GNU General Public License v3.0' |
+    'MIT License' |
+    'BSD 2-Clause "Simplified" License' |
+    'BSD 3-Clause "New" or "Revised" License' |
+    'Boost Software License 1.0' |
+    'Creative Commons Zero v1.0 Universal' |
+    'Eclipse Public License 2.0' |
+    'GNU Affero General Public License v3.0' |
+    'GNU General Public License v2.0' |
+    'GNU Lesser General Public License v2.1' |
+    'Mozilla Public License 2.0' |
+    'The Unlicense'
+
+type TInstallation =
+    'npm' |
+    'yarn' |
+    'bun'
+
 interface ICreateOptions {
     email: string;
     github: string;
@@ -31,9 +52,9 @@ interface ICreateOptions {
     build: string;
     solve: string;
     learn: string;
-    installation: string;
+    installation: TInstallation;
     usage: string;
-    license: string;
+    license: TLicense;
 }
 
 interface IPrompts {
@@ -41,5 +62,10 @@ interface IPrompts {
     message: string,
     default?: string,
     type?: TPromptType,
-    choices?: Array<string>
+    choices?: Array<TLicense> | Array<TInstallation>
+}
+
+interface ILicense { 
+    badge: string,
+    link: string
 }
